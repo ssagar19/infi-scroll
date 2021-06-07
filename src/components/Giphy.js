c94908f44e067d9ee1d85258ff73ecc649485327
ssagar19 <sagarshubham47@gmail.com>
2021-06-07 20:03:57 +0530

    infi done

--- src/components/Giphy.js
+++ src/components/Giphy.js
@@ -6,32 +6,29 @@ import InfiniteScroll from "react-infinite-scroll-component";
 const Giphy = () => {
     const [search, setSearch] = useState("");
     const [images, setImages] = useState([]);
-
     const [page, setPage] = useState(1);
-
     useEffect(() => {
-        console.log(page);
-        if (search) {
-            Axios.get(
-                `https://api.giphy.com/v1/gifs/search?api_key=hZ7mmre1MO3P756r2fAh5lCKNGrGrKdE&q=${search}&offset=${page}&limit=${10}`
-            ).then((res) => {
-                console.log(res.data.data);
-                setImages((prev) => [...prev, ...res.data.data]);
-            });
-        } else {
-            Axios.get(
-                `https://api.giphy.com/v1/gifs/trending?api_key=hZ7mmre1MO3P756r2fAh5lCKNGrGrKdE&offset=${page}&limit=${10}`
-            ).then((res) => {
-                setImages((prev) => [...prev, ...res.data.data]);
-            });
-        }
+        fetchData();
     }, [page]);
 
-    function handleSearchChange(e) {
-        console.log("hi");
-        setSearch(e.target.value);
-        console.log(search);
+    async function fetchData() {
+        if (search) {
+            const res = await Axios.get(
+                `https://api.giphy.com/v1/gifs/search?api_key=hZ7mmre1MO3P756r2fAh5lCKNGrGrKdE&q=${search}&offset=${page}&limit=${10}`
+            );
+            setImages((prev) => [...prev, ...res.data.data]);
+        } else {
+            const res = await Axios.get(
+                `https://api.giphy.com/v1/gifs/trending?api_key=hZ7mmre1MO3P756r2fAh5lCKNGrGrKdE&offset=${page}&limit=${10}`
+            );
+            setImages((prev) => [...prev, ...res.data.data]);
+        }
     }
+
+    function handleSearchChange(e) {
+        setSearch(e.target.value);
+    }
+
     async function handleSubmit(e, search) {
         console.log(search);
         setImages([]);
@@ -42,8 +39,8 @@ const Giphy = () => {
             console.log(res);
             setImages((prev) => [...prev, ...res.data.data]);
         });
-        //setSearch(search);
     }
+
     return (
         <div className="m-2">
             <h1 className="banner">Giphisix</h1>
@@ -55,10 +52,7 @@ const Giphy = () => {
             <div className="container gifs">
                 <InfiniteScroll
                     dataLength={images.length}
-                    next={() => {
-                        // setSearch((search) => search);
-                        setPage((page) => page + 10);
-                    }}
+                    next={() => setPage((page) => page + 10)}
                     hasMore={true}
                 >
                     {images.map((el) => (
