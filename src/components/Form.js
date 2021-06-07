c55768e874754ddb4dc8cdedc29cd77d16907d3a
ssagar19 <sagarshubham47@gmail.com>
2021-06-07 18:56:45 +0530

    inf scroll

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,14 +1,18 @@
-const Form = (props) => {
+const Form = ({ search, handlesubmit, handleSearch }) => {
     return (
         <form className="form-inline justify-content-center m-2 search-form">
             <input
-                value={props.search}
-                onChange={props.handleSearchChange}
+                value={search}
+                onChange={(e) => handleSearch(e)}
                 type="text"
-                placeholder="search"
+                placeholder="search for more Gifs"
                 className="form-control"
             />
-            <button type="submit" className="btn btn-primary mx-2">
+            <button
+                type="submit"
+                className="btn btn-primary mx-2"
+                onClick={(e) => handlesubmit(e, search)}
+            >
                 Search
             </button>
         </form>
--- src/components/Giphy.js
+++ src/components/Giphy.js
@@ -1,20 +1,64 @@
-import { useState } from "react";
+import Axios from "axios";
+import { useState, useEffect } from "react";
 import Form from "./Form";
-import useFetchImage from "./useFetchImage";
 import InfiniteScroll from "react-infinite-scroll-component";
 
-const Giphy = (count) => {
-    const [page, setPage] = useState(0);
-    const [images, setImages] = useFetchImage(page);
+const Giphy = () => {
+    const [search, setSearch] = useState("");
+    const [images, setImages] = useState([]);
 
+    const [page, setPage] = useState(1);
+
+    useEffect(() => {
+        console.log(page);
+        if (search) {
+            Axios.get(
+                `https://api.giphy.com/v1/gifs/search?api_key=hZ7mmre1MO3P756r2fAh5lCKNGrGrKdE&q=${search}&offset=${page}&limit=${10}`
+            ).then((res) => {
+                console.log(res.data.data);
+                setImages((prev) => [...prev, ...res.data.data]);
+            });
+        } else {
+            Axios.get(
+                `https://api.giphy.com/v1/gifs/trending?api_key=hZ7mmre1MO3P756r2fAh5lCKNGrGrKdE&offset=${page}&limit=${10}`
+            ).then((res) => {
+                setImages((prev) => [...prev, ...res.data.data]);
+            });
+        }
+    }, [page]);
+
+    function handleSearchChange(e) {
+        console.log("hi");
+        setSearch(e.target.value);
+        console.log(search);
+    }
+    async function handleSubmit(e, search) {
+        console.log(search);
+        setImages([]);
+        e.preventDefault();
+        Axios.get(
+            `https://api.giphy.com/v1/gifs/search?api_key=hZ7mmre1MO3P756r2fAh5lCKNGrGrKdE&q=${search}&offset=${page}&limit=${10}`
+        ).then((res) => {
+            console.log(res);
+            setImages((prev) => [...prev, ...res.data.data]);
+        });
+        //setSearch(search);
+    }
     return (
         <div className="m-2">
             <h1 className="banner">Giphisix</h1>
-            <Form />
+            <Form
+                handleSearch={handleSearchChange}
+                search={search}
+                handlesubmit={handleSubmit}
+            />
             <div className="container gifs">
                 <InfiniteScroll
                     dataLength={images.length}
-                    next={() => setPage(page + 5)}
+                    next={() => {
+                        // setSearch((search) => search);
+                        setPage((page) => page + 10);
+                    }}
                     hasMore={true}
                 >
                     {images.map((el) => (
